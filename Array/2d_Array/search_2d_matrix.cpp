926ca1e0bb76b87db5a73e4ba365e146d7e26b5f
Sristi1123 <sristi1501@gmail.com>
2026-06-23 15:30:52 +0530

    save

--- Array/2d_Array/search_2d_matrix.cpp
+++ Array/2d_Array/search_2d_matrix.cpp
@@ -1,15 +1,29 @@
-// https://leetcode.com/problems/search-a-2d-matrix/
+// // https://leetcode.com/problems/search-a-2d-matrix/
 
 // class Solution {
 // public:
 //     bool searchMatrix(vector<vector<int>>& matrix, int target) {
 //         int n=matrix.size();
 //         int m=matrix[0].size();
-//         for(int i=0;i<n;i++){
-//             for(int j=0;j<m;j++){
-//                 if(matrix[i][j]==target){
-//                     return true;
-//                 }
+//         // for(int i=0;i<n;i++){
+//         //     for(int j=0;j<m;j++){
+//         //         if(matrix[i][j]==target){
+//         //             return true;
+//         //         }
+//         //     }
+//         // }
+//         int start=0;
+//         int end=m*n-1;
+//         while(start<=end){
+//             int mid=start+(end-start)/2;
+//             int row=mid/m;
+//             int col=mid%m;
+//             if(matrix[row][col]==target){
+//                 return true;
+//             }else if(matrix[row][col]>target){
+//                 end=mid-1;
+//             }else{
+//                 start=mid+1;
 //             }
 //         }
 //         return false;
